c46eefaa2d115f19ac78d2e21f5069b519905626
FlorianHH <791160+FlorianHH@users.noreply.github.com>
2023-08-04 15:02:46 +0200

    freecad: update arm sha256

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -2,7 +2,7 @@ cask "freecad" do
   arch arm: "arm64", intel: "intel_x86"
 
   version "0.21.0"
-  sha256 arm:   "e12232d2f3411966f25837a4d5f2c14b72ea700b2a4dbb0a7b88f6b1da3044fb",
+  sha256 arm:   "7e75a2fdc7588ba8d8f0effc415516128f70b8e1d00c808804b6e0071b233925",
          intel: "b5c938fe1f3356a7571de6289c804316701459a224c498e7ad09589db31ee7ba"
 
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version}/FreeCAD-#{version}-mac-#{arch}.dmg",
